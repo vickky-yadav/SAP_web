17a554b2e751f21d720d9a05239198b94d157f74
aditya-mag <114802597+aditya-mag@users.noreply.github.com>
2022-10-19 12:27:42 +0530

    boi changes

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Demo from "./components/signup";
+
+ReactDOM.render(
+  <>
+    <Demo />
+  </>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
